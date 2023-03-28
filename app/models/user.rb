3fac610d5dc61b78fff7c2ab4872fbc5e9cf3574
nari19940414 <125851630+nari19940414@users.noreply.github.com>
2023-03-28 12:32:16 +0900

    テストコードの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  
-  validates :encrypted_password,:password,:password_confirmation,length:{minimum:6},format:{with: /(?=.[a-zA-Z])(?=.\d)[a-zA-Z\d]{6,}/}
+  validates :encrypted_password,:password,:password_confirmation,length:{minimum:6},format:{with: /(?=.[a-zA-Z])(?=.\d)[a-zA-Z\d]{6,}/} #半角英数字6文字以上
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } #名前苗字
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } #かな
+  validates :birth_date, presence: true #生年月日
+
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,49 @@
-  require 'rails_helper'
- RSpec.describe User, type: :model do
-   describe "ユーザー新規登録" do
-     it "nicknameが空だと登録できない" do
-     end
-     it "emailが空では登録できない" do
-     end
-   end
- end
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  describe "ユーザー新規登録" do
+    it "nicknameが空だと登録できない" do
+      user = User.new(nickname: '', email: 'kkk@gmail.com', password: '00000000', password_confirmation: '00000000')
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+      
+    end
+    it "パスワードが空では登録できない" do
+      user = User.new(nickname: 'abe', email: 'kkk@gmail.com', password: '', password_confirmation: '00000000')
+      user.valid?
+      expect(user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'パスワードが6文字以上でなければ登録できない' do
+      user = User.new(nickname: 'abe', email: 'testmail', password: '12345', password_confirmation: '12345')
+      user.valid?
+      expect(user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end    
+
+    it 'パスワードとパスワード（確認）が一致しない場合、登録できない' do
+      user = User.new(nickname: 'abe', email: 'testmail', password: 'password', password_confirmation: 'different_password')
+      user.valid?
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'メールアドレスが空では登録できない' do
+      user = User.new(nickname: 'abe', email: '', password: 'password', password_confirmation: 'password')
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
+    end
+    
+
+    it "メールアドレスが重複すると登録できない" do
+      user = User.create(nickname: 'abe', email: '@user.email', password: '', password_confirmation: '00000000')
+      user.valid?
+      expect(user.errors.full_messages).to include("Email is invalid")
+    end
+
+    it 'メールアドレスは、@を含まないと登録できない' do
+      user = User.new(nickname: 'abe', email: 'testmail', password: 'password', password_confirmation: 'password')
+      user.valid?
+      expect(user.errors.full_messages).to include('Email is invalid')
+    end
+
+
+  end
+end
