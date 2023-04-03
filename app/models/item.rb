e9ee946207d3bc0b45f042aa701a44881be7c5b1
nari19940414 <125851630+nari19940414@users.noreply.github.com>
2023-04-03 19:17:54 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   belongs_to :city
   belongs_to :delivery_span
   has_one_attached :image
-
+  validates :user, presence: true
   validates :product_name, :product_description, :image, presence: true
   validates :category_id, :condition_id, :delivery_pay_id, :city_id, :delivery_span_id, numericality: { other_than: 1 , message: "can't be blank" } 
   validates :product_price, presence: true, numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range' } do
