a14d066533cef859fa0cd4cb1268c864e5fda759
nari19940414 <125851630+nari19940414@users.noreply.github.com>
2023-04-04 18:08:37 +0900

    ユーザーによって表示されるボタンが変化するように条件分岐する

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to:'items#index'
-  resources :items
-    
+  resources :items do
+  resources :orders, only: [:new, :create]
+  end
 
 end
