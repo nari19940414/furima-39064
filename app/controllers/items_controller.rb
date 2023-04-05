841341873043c20a6d85c201399405abc64de93a
nari19940414 <125851630+nari19940414@users.noreply.github.com>
2023-04-05 16:44:56 +0900

    再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,7 +37,8 @@ class ItemsController < ApplicationController
     end
     
     def destroy
-       if @item.destroy
+      if @item.user == current_user 
+        @item.destroy
         redirect_to root_path
        else
          render :edit
